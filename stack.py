d615a1fd4bb0170f648aebc2c318d30d7538887e
Aynaz Khasanov <lordofmask@gmail.com>
2023-03-11 23:54:00 +0500

    another some changes

--- stack.py
+++ stack.py
@@ -69,6 +69,39 @@ class LinkedList:
             node = node.Next
         return 'End of List'
         
+class MaxStack:
+    def __init__(self):
+        self.main = []
+        self.max = []
+
+    def push(self, data):
+        if len(self.main) == 0:
+            self.max.append(data)
+        elif data > self.max[-1]:
+            self.max.pop()
+            self.max.append(data)
+        self.main.append(data)
+
+    def pop(self):
+        return self.main.pop()
+
+    def get_max(self):
+        return self.max[-1]
+
+def checkBalance(a_string: str) -> bool:
+    if not a_string: return False
+    else:
+        stack = [] 
+        for c in a_string:
+            if c == '(' or c == '{':
+                stack.append(c)
+            elif c == ')' or c == '}':
+                try:
+                    stack.pop()
+                except:
+                    return False
+    return len(stack) == 0
+
 
 a_list = LinkedList()
 
@@ -76,4 +109,11 @@ a_list.append('Monday')
 a_list.append('Tuesday')
 a_list.append('Wendesday')
 print('Final list:', a_list)
+
+lst = MaxStack()
+lst.push(1)
+lst.push(3)
+lst.push(0)
+a = 'ksfkgaa(ksfldf)l;sdlfl(lksdg{});sk'
+print(checkBalance(a))
 #a_list.reverse_list()
